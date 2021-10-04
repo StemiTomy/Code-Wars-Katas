ea7e5d3071a6a90a4c88cbaf88a33604000949f3
Stelut Tomoiaga <stemitomy@gmail.com>
2021-10-05 01:33:10 +0200

    Solución a 8kyu Calculate BMI
    Según el peso y la altura, te devuelve un mensaje sabiendo si tienes sobrepeso o si estás normal, etc.

---
+++ 8kyu_Calculate_BMI.java
@@ -0,0 +1,14 @@
+public class Calculate {
+  // la mejor practica:
+  public static String bmi(double weight, double height) {
+    double bmi = weight/(height*height);
+    if (bmi<=18.5){
+      return "Underweight";
+    } else if (bmi<=25.0){
+      return "Normal";
+    } else if (bmi<=30.0){
+      return "Overweight";
+    } 
+    return "Obese";
+  }
+}
